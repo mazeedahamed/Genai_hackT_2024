c7952e30ea39fd1d94a522b6eef8ec7cbb8ee284
mazeedahamed <41569897+mazeedahamed@users.noreply.github.com>
2025-03-12 21:48:26 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 import pandas as pd
+from datetime import datetime
 
 # Load Excel files
 excel1_path = "excel1.xlsx"  # Update with actual path
@@ -18,27 +19,28 @@ df2['occurrence'] = df2.groupby(group_by_columns).cumcount()
 merged = df1.merge(df2, on=group_by_columns + ['occurrence'], how='left', suffixes=('', '_ai'))
 
 # Add ai_response_parties column
-merged['ai_response_parties'] = merged['party name']
-merged.loc[merged['party name_ai'].isna(), 'ai_response_parties'] = ''
+merged['ai_response_parties'] = merged['party name_ai'].fillna('')
 
 # Find unmatched records in df2 per account
 unmatched_records = df2[~df2.set_index(group_by_columns + ['occurrence']).index.isin(df1.set_index(group_by_columns + ['occurrence']).index)]
 
 # Group unmatched records by account and append them under the respective account
 new_rows = []
+
 for account, group in unmatched_records.groupby('account no'):
-    empty_row = pd.DataFrame([[account] + [''] * (len(df1.columns) - 1)], columns=df1.columns)
-    new_group = group.rename(columns={'party name': 'ai_response_parties'})
-    new_group = new_group[['account no', 'ai_response_parties']]
+    new_rows.append(pd.DataFrame([[account] + [''] * (len(df1.columns) - 1)], columns=df1.columns))  # Empty separator row
+    new_group = group.rename(columns={'party name': 'ai_response_parties'})[['account no', 'ai_response_parties']]
     new_group = new_group.reindex(columns=df1.columns, fill_value='')
-    new_rows.append(empty_row)
     new_rows.append(new_group)
 
 new_rows_df = pd.concat(new_rows, ignore_index=True) if new_rows else pd.DataFrame(columns=df1.columns)
 
 # Combine merged data with newly formatted unmatched records
-final_df = pd.concat([merged, new_rows_df], ignore_index=True)
-final_df.drop(columns=['occurrence'], inplace=True)
+final_df = pd.concat([merged, new_rows_df], ignore_index=True).drop(columns=['occurrence'])
 
-# Save output to Excel
-final_df.to_excel("merged_output.xlsx", index=False)
+# Save output with a timestamped filename
+timestamp = datetime.now().strftime("%Y%m%d_%H%M%S")
+output_filename = f"merged_output_{timestamp}.xlsx"
+final_df.to_excel(output_filename, index=False)
+
+print(f"File saved as: {output_filename}")
